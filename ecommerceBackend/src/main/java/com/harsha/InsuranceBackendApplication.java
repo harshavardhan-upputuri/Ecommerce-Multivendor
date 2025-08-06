a8030703cf20238c3f6f81e14d370ca0de373b90
harshavardhan-upputuri <harshaupputuri123@gmail.com>
2025-08-06 21:57:14 +0530

    Backend

--- ecommerceBackend/src/main/java/com/harsha/InsuranceBackendApplication.java
+++ ecommerceBackend/src/main/java/com/harsha/InsuranceBackendApplication.java
@@ -1,13 +1,31 @@
+// package com.harsha;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class InsuranceBackendApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(InsuranceBackendApplication.class, args);
+// 	}
+
+// }
+
 package com.harsha;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InsuranceBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InsuranceBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Load .env variables and set them as system properties
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
+        SpringApplication.run(InsuranceBackendApplication.class, args);
+    }
 }
